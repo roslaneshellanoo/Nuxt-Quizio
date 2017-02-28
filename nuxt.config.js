0ed78df6bc4854fcdeb5b5bd648ad4d8e5793127
roslanelyashiv <roslane@shellanoo.com>
2017-03-01 01:22:23 +0200

    muse ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,52 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'Quizio.io - Test your code skills',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Nuxt.js project'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto'}
     ]
   },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
-
-    { src: '~assets/css/main.scss', lang: 'scss' }
+    {src: 'muse-ui/dist/muse-ui.css'},
+    {src: '~assets/css/main.scss', lang: 'scss'}
   ],
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3a5783' },
+   ** Customize the progress-bar color
+   */
+  loading: {color: '#3a5783'},
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
+  plugins: [
+    //'~plugins/muse-ui'
+  ],
+
   build: {
+
+    vendor: ['muse-ui'],
     /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+     ** Run ESLINT on save
+     */
+    // extend (config, ctx) {
+    //   if (ctx.isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
   }
 }
